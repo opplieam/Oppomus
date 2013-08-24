1fe524d16bad2ede6c323c82dc2cce88d89d45fc
Nattaphoom Ch <attomos@gmail.com>
2013-08-24 21:04:02 +0700

    Auto create logs directory if it is not exist and make run.py executable.

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python
 from controllers.main import main
+import os
+
+logs_dir = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'logs')
 
 
 def run():
+    print logs_dir
+    if not os.path.isdir(logs_dir):
+        print ("Creating %s" % logs_dir)
+        os.makedirs(logs_dir)
+
     main()
 
 run()
