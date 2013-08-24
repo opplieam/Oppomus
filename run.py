d102f1f65ac62d01dca4a0d517580586a369fdb4
Nattaphoom Ch <attomos@gmail.com>
2013-08-24 22:09:24 +0700

    Delete print logs directory line

--- run.py
+++ run.py
@@ -6,7 +6,6 @@ logs_dir = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'logs')
 
 
 def run():
-    print logs_dir
     if not os.path.isdir(logs_dir):
         print ("Creating %s" % logs_dir)
         os.makedirs(logs_dir)
